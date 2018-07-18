160d9fd6e541a37fb1001333f61f433549d05fad
Matthew <35214655+havadtoi@users.noreply.github.com>
2018-07-18 22:18:43 +0100

    v0.3
    Included randomised market rates for the second chart as a pinch of additional realism.

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,14 @@
 #importing libraries used in the code
 import matplotlib.pyplot as plt #Used for charting
+import numpy as np
 
 #Setting parameters for calculations
 HousePrice = 500000         #Value of the house to be purchased
 Deposit = 100000            #Initial downpayment for the mortgage
 InterestRate = 0.036        #Annual interest rate on the Mortgage
 Duration = 25               #Number of years of the mortgage contract
+
+#Market Rates and estimates
 HouseMarketRate = 0.0745      #Expected average house price index growth rate / UK Average is 7.45% for London Flats between '91 - '14
 InvestmentRate = 0.1185      #Expected average investment growth rate / S&P500 Index average is 11.85% between '70 and 2017
 
@@ -30,23 +33,47 @@ for value in range(1, Duration +1) :
 
 NetProfit = [HouseNetPnL[value] - AlternativeInvestmentCost[value] for value in range(Duration + 1)]
 
-#Charting the Calculated lists
-#plt.plot(Equity, label = 'Equity')
-#plt.plot(MortgageBalance, label = 'Mortgage Balance')
-#plt.plot(InterestPaid, label = 'InterestPaid')
-#plt.plot(HouseValue, label = 'House Value')
-#plt.plot(LoanToEquity, label = 'Loan to Equity')
-#plt.plot(NetValueOwned, label = 'Net Value Owned')
-#plt.plot(HouseMtM_PnL, label = 'House MtM P&L')
-#plt.plot(HouseNetPnL, label = 'House Net MtM P&L')
-#plt.plot(AlternativeInvestmentCost, label = 'Alternative Investment Cost')
+#Charting the final calculation
 plt.plot(NetProfit, label = 'Net Mortgage P&L')
 
-plt.title('Mortgage Analysis')
+plt.title('Mortgage Analysis in an Ideal World')
 plt.grid(True)
 plt.ylabel('Currency')
 plt.xlabel('Years')
 plt.legend()
 plt.show()
 
+
+
+### 2nd Section
+
+
+
+#Testing the whole calculation with randomized market movements
+np.random.seed()
+
+for x in range(10) :
+    InvestmentRatesYearly = [np.random.triangular(-10,(InvestmentRate * 100),15) / 100 for value in range(Duration + 1)]
+    HouseMarketRatesYearly = [np.random.triangular(-5,(HouseMarketRate * 100),10) / 100 for value in range(Duration + 1)]
+    HouseValue = [HousePrice * (1 + HouseMarketRatesYearly[value]) ** value for value in range(Duration + 1)]     #House price appreciation using the random market rate generator
+    LoanToEquity = [Equity[value]/HouseValue[value] * 100 for value in range(Duration + 1)]         #Loan to Equity development
+    NetValueOwned = [HouseValue[value] - MortgageBalance[value] for value in range(Duration + 1)]   #Net value owned in the house at the estimated market price
+    HouseMtM_PnL = [HouseValue[value] - HousePrice for value in range(Duration + 1)]                #Profi&Loss on the house value
+    HouseNetPnL = [HouseMtM_PnL[value] - InterestPaid[value] for value in range(Duration + 1)]      #Profit&Loss on the whole project
+    #Cumulative alternative investment cost of the equity paid in to the house
+    AlternativeInvestmentCost = list([0 for value in range(Duration +1)])
+    for value in range(1, Duration +1) :
+        AlternativeInvestmentCost[value] = AlternativeInvestmentCost[value-1] + AlternativeInvestmentCost[value-1] * InvestmentRatesYearly[value] + Equity[value] * InvestmentRate #using the random market rate generator
+    NetProfit = [HouseNetPnL[value] - AlternativeInvestmentCost[value] for value in range(Duration + 1)]
+    #Charting the final calculation
+    plt.plot(NetProfit, label = 'Net Mortgage P&L')
+
+
+plt.title('Mortgage Analysis with some level of realism')
+plt.grid(True)
+plt.ylabel('Currency')
+plt.xlabel('Years')
+plt.show()
+
 #ToDo: Create 3D graphics on the house market rate and investment rate affects
+
